ea0543ec5103c7d56ae92d1e8f013aa328c8b39d
Josh Patiño <josh.patinof@gmail.com>
2026-06-10 15:55:12 +0800

    First CSES problem

---
+++ CSES/IntroductoryProblems/WeirdAlgorithm.cpp
@@ -0,0 +1,21 @@
+// written by josh.patinof
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n;
+    if (!(cin >> n))
+        return 0;
+
+    while (n != 1)
+    {
+        cout << n << " ";
+        if (n % 2 == 0)
+            n /= 2;
+        else
+            n = n * 3 + 1;
+    }
+    cout << 1 << endl;
+    return 0;
+}
