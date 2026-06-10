45867ba659a7eaeefbe87ca72a7f247b1c1896cf
hsojfp <josh.patinof@gmail.com>
2026-06-10 20:31:58 +0800

    to be solved

---
+++ Codeforces/1370/D.cpp
@@ -0,0 +1,26 @@
+// written by josh.patinof June 5, 2026
+// D. Odd-Even Subsequence - https://codeforces.com/problemset/problem/1370/D
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+void solve() {
+    ll n, k; cin >> n >> k;
+    vector<ll> a(n);
+    for (auto &x: a){
+        cin >> x;
+    }
+     
+}
+
+int main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
