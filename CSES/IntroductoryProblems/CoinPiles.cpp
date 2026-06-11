813b296220cb08f3a858e78c99c3eea54727235b
hsojfp <josh.patinof@gmail.com>
2026-06-11 21:20:47 +0800

    to be continued

---
+++ CSES/IntroductoryProblems/CoinPiles.cpp
@@ -0,0 +1,21 @@
+// written by josh.patinof
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+void solve(ll a, ll b) {
+    
+}
+
+int main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        ll a, b; cin >> a >> b;
+        solve(a, b);
+    }
+    return 0;
+}
