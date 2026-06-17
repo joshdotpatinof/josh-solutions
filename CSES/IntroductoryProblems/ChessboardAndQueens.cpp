9aaf5f6a2b2ecbd1f2a7cdcbec0db5dedb7e7ba1
hsojfp <josh.patinof@gmail.com>
2026-06-17 21:18:28 +0800

    to solve

---
+++ CSES/IntroductoryProblems/ChessboardAndQueens.cpp
@@ -0,0 +1,24 @@
+// written by josh.patinof
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+void solve() {
+    
+}
+
+int main() {
+    fast_io;
+    //int t = 1;
+    //cin >> t;
+    //while (t--) {
+    vector<string> grid(8);
+    for (auto &x: grid){
+        cin >> x;
+    }
+    solve();
+    //}
+    return 0;
+}
