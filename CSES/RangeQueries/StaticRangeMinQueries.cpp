83ec27233ee08dc4a53aaaf13cc0bc93f0d8a9de
hsojfp <josh.patinof@gmail.com>
2026-06-20 11:40:57 +0800

    to be solved

---
+++ CSES/RangeQueries/StaticRangeMinQueries.cpp
@@ -0,0 +1,44 @@
+// written by josh.patinof
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+void _build(vector<ll> &tree){
+
+}
+void solve() {
+    ll n, q; cin >> n >> q;
+    vector<ll> a(n);
+    for (auto &x: a){
+        cin >> x;
+    }
+    vector<ll> tree();
+    while (q--){
+        ll a, b; cin >> a >> b;
+
+    }
+}
+
+int main() {
+    fast_io;
+    //int t = 1;
+    //cin >> t;
+    //while (t--) {
+    solve();
+    //}
+    return 0;
+}
+
+
+//        1
+//       1 1
+//      1 1 1
+//     1 1 1 1 
+//    2 1 1 1 1
+//   2 2 1 1 1 1
+//  2 2 4 1 1 1 3
+// 3 2 4 5 1 1 5 3
+
+
