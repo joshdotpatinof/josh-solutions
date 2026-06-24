d8ae27d56de6f740b77621b656e6856026ce2aff
hsojfp <josh.patinof@gmail.com>
2026-06-24 23:00:27 +0800

    solved

---
+++ AtCoder/Easy-100/ABC068B.cpp
@@ -0,0 +1,28 @@
+// written by josh.patinof
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+void solve() {
+    int n; cin >> n;
+    ll ans = n;
+    while (ans >= 1){
+        if (ans > 0 and (ans & (ans - 1)) == 0){
+            break;   
+        }
+        ans--;
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    fast_io;
+    //int t = 1;
+    //cin >> t;
+    //while (t--) {
+    solve();
+    //}
+    return 0;
+}
