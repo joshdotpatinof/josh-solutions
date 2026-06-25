5a9047dde52bbd78b5332be66df40d45b8ebd052
hsojfp <josh.patinof@gmail.com>
2026-06-25 11:26:01 +0800

    to solve

---
+++ AtCoder/Easy-100/ABC161C.cpp
@@ -0,0 +1,23 @@
+// written by josh.patinof
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+void solve() {
+    ll n, k; cin >> n >> k;
+    // find an x that gives the minimum(?) diff |x - K| 0 <= x <= N
+    ll min_diff = LONG_LONG_MAX;
+    cout << min_diff << endl;
+}
+
+int main() {
+    fast_io;
+    //int t = 1;
+    //cin >> t;
+    //while (t--) {
+    solve();
+    //}
+    return 0;
+}
