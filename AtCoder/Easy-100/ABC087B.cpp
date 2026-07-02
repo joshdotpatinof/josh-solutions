d9d6a7063c4c3f4df8fee9597774a31cc77ea9f3
hsojfp <josh.patinof@gmail.com>
2026-07-02 20:49:11 +0800

    to be continued

---
+++ AtCoder/Easy-100/ABC087B.cpp
@@ -0,0 +1,27 @@
+// written by josh.patinof
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+void solve() {
+    int a, b, c; cin >> a >> b >> c;
+    int x; cin >> x;
+    vector<int> dp(x + 1);
+    for (int i = 1; i <= x; ++i){
+        
+    }
+
+    cout << dp[x] << endl;
+}
+
+int main() {
+    fast_io;
+    //int t = 1;
+    //cin >> t;
+    //while (t--) {
+    solve();
+    //}
+    return 0;
+}
