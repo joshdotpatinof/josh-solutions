1a56fdee70608018e3b92060ea9a9a902f888e9a
hsojfp <josh.patinof@gmail.com>
2026-07-02 12:30:38 +0800

    solved

---
+++ AtCoder/Easy-100/ABC158B.cpp
@@ -0,0 +1,25 @@
+// written by josh.patinof
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+void solve() {
+    ll n, a, b; cin >> n >> a >> b;
+    ll ans = n / (a + b) * a;
+    ll rem = n % (a + b);
+    ans += min(rem, a);
+
+    cout << ans << endl;
+}
+
+int main() {
+    fast_io;
+    //int t = 1;
+    //cin >> t;
+    //while (t--) {
+    solve();
+    //}
+    return 0;
+}
