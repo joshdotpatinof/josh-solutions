447ff9d508618670a042ec15a48cb06c2d725994
hsojfp <josh.patinof@gmail.com>
2026-07-11 20:50:06 +0800

    to solve

---
+++ AtCoder/Easy-100/ABC115C.cpp
@@ -0,0 +1,32 @@
+// written by josh.patinof
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+void solve() {
+    int n, k; cin >> n >> k;
+    vector<int> h(n);
+    for (auto &x: h){
+        cin >> x;
+    }
+    
+    // to be cont
+    // create static min array
+    // create static max array
+    // get min and max in O(1) or O(log n) time goal
+    // Sliding window (K - size);
+
+    
+}
+
+int main() {
+    fast_io;
+    //int t = 1;
+    //cin >> t;
+    //while (t--) {
+    solve();
+    //}
+    return 0;
+}
