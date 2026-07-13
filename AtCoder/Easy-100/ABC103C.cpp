5ec8b48977b05067cb1ce04f58cbe03594ca6971
hsojfp <josh.patinof@gmail.com>
2026-07-13 17:54:02 +0800

    solved

---
+++ AtCoder/Easy-100/ABC103C.cpp
@@ -0,0 +1,29 @@
+// written by josh.patinof
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+void solve() {
+    int n; cin >> n;
+    vector<int> a(n);
+    for (auto &x : a){
+        cin >> x;
+    }
+    int sum = 0;
+    for (auto x : a){
+        sum += (x - 1);
+    }
+    cout << sum << endl;
+}
+
+int main() {
+    fast_io;
+    //int t = 1;
+    //cin >> t;
+    //while (t--) {
+    solve();
+    //}
+    return 0;
+}
