3c80edfc9df8cf4f555b8924d7a3d3d4d8e0cb43
hsojfp <josh.patinof@gmail.com>
2026-07-18 00:20:52 +0800

    tbc

--- CSES/IntroductoryProblems/ChessboardAndQueens.cpp
+++ CSES/IntroductoryProblems/ChessboardAndQueens.cpp
@@ -5,8 +5,20 @@ using namespace std;
 using ll = long long;
 #define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
 
-void solve() {
-    
+ll solve(int i, vector<string> &grid) {
+    if (i == 8){
+        return 0; // ans
+    }
+
+    for (int j = 0; j < 8; ++j){
+        if (grid[i][j] != '*'){
+            // can only put queens in free
+            // try putting
+        }
+
+    }
+
+    return ways;
 }
 
 int main() {
@@ -18,7 +30,7 @@ int main() {
     for (auto &x: grid){
         cin >> x;
     }
-    solve();
+    cout << solve(0, grid) << endl;
     //}
     return 0;
 }
